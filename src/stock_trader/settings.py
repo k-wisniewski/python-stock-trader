f6760b09d7d112c22823faadcb24e20775cd8f5a
Krzysztof Wiśniewski <krzysztof.wisniewski@codility.com>
2023-10-18 19:31:12 +0200

    Add testing settings

--- src/stock_trader/settings.py
+++ src/stock_trader/settings.py
@@ -2,6 +2,7 @@ from enum import Enum, auto
 from pydantic import DirectoryPath
 from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
+import sys
 
 
 class Concurrency(Enum):
@@ -10,7 +11,9 @@ class Concurrency(Enum):
     ASYNCIO = auto()
     SINGLE_THREADED = auto()
 
-ENV_FILE = Path(__file__).parent / ".env"
+ENV_FILE = Path(__file__).parent / ".env_run"
+if "pytest" in sys.modules:
+    ENV_FILE = Path(__file__).parent / ".env_test"
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="STOCK_TRADER_", env_file=ENV_FILE, env_file_encoding="utf-8")
     report_output_path: DirectoryPath
